3c41c61ad2abdb06ebf4f003946144364d3ab20c
IsaacLefebvre <i.lefebvre@kaisostudio.org>
2025-02-27 15:40:47 +0100

    carrousel debug

--- src/script.js
+++ src/script.js
@@ -34,57 +34,6 @@ $("#btn_eng").click(() => {
     translate();
 })
 
-
-
-//Header onScroll display none--------------------------------------------------------------------------------------
-$(window).on("scroll", function () {
-    if ($(window).scrollTop() === 0) {
-        $("header .logo").show();
-        header.style.justifyContent = "space-between";
-    } else {
-        $("header .logo").hide();
-        header.style.justifyContent = "flex-end";
-    }
-
-    if ($(window).scrollTop() > $("#newHeader").offset().top - 80) {
-        document.querySelectorAll(".btn_header").forEach((button) => {
-            button.classList.add("blue");
-            button.classList.remove("white");
-        });
-        document.querySelector("header nav").classList.add("bg_header");
-        document.querySelector("header nav").classList.add("drop_shadow");
-    } else {
-        document.querySelectorAll(".btn_header").forEach((button) => {
-            button.classList.remove("blue");
-            button.classList.add("white");
-        });
-        document.querySelector("header nav").classList.remove("bg_header");
-        document.querySelector("header nav").classList.remove("drop_shadow");
-    }
-});
-
-if (!window.location.href.includes('index')) {
-    document.querySelectorAll(".btn_header").forEach((button) => {
-        button.classList.add("blue");
-        button.classList.remove("white");
-    });
-    document.querySelector("header nav").classList.add("bg_header");
-    document.querySelector("header nav").classList.add("drop_shadow");
-}
-
-//Burger Menu--------------------------------------------------------------------------------------
-$("#icons").click(() => {
-    document.querySelector("#header").classList.toggle("active");
-})
-
-document.querySelectorAll("header nav li").forEach((link) => {
-    link.addEventListener("click", () => {
-        header.classList.remove("active");
-    });
-});
-
-
-
 //Swiper menu--------------------------------------------------------------------------------------
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
