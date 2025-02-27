bcc8d6eb18fe97ee13446d5c2fe42203fdcff6c8
Stevi60 <tazmo634@gmail.com>
2025-02-27 19:03:00 +0100

    Finished the text of each price box

--- src/custom-prices.js
+++ src/custom-prices.js
@@ -9,9 +9,16 @@ template.innerHTML = `
 	<div class = "drop_shadow center" style = "margin: 2rem 0; height: 16rem" >
 		<div class = "margin" style = "display: flex; justify-content: space-between">
 			<div class = "margin" style = "display: flex; flex-direction: column">
+				<div style = "display: flex; margin-bottom: 0.5rem">
+					<p class = "violet"><b>Logo</b></p>
+					<p class = "violet" style = "margin: 0 0.5rem"><b>Charte Graphique</b></p>
+				</div>
+				
 				<h2 class = "fr">Branding</h2>
+
 				<p class = "fr" style = "margin-bottom: 1rem">Nous vous aidons à positionner votre marque selon les préférences de vos clients.</p>
-				<a class = "btn btn_base fr" style = "margin-right: 2rem">Lançons votre projet  &#10140;</a>
+
+				<a class = "btn btn_base center fr" style = "padding: 0.5rem 0.5rem; width: 10rem">Lançons votre projet  &#10140;</a>
 			</div>
 
 			<div class = "margin">
@@ -23,27 +30,44 @@ template.innerHTML = `
 	</div>
 
 	<div class = "drop_shadow center" style = "margin: 2rem 0; height: 16rem" >
-		<div class = "margin" style = "display: flex; justify-content: space-between">
-			<div class = "margin">
-			</div>
-
-			<div class = "margin">
-			</div>
-
+		<div class = "margin" style = "display: flex; flex-direction: row-reverse; justify-content: space-between">
 			<div class = "margin" style = "display: flex; flex-direction: column; text-align: right">
+				<div style = "display: flex; flex-direction: row-reverse; margin-bottom: 0.5rem;">
+					<p class = "violet"><b>Site Web</b></p>
+					<p class = "violet" style = "margin: 0 0.5rem"><b>Prototypage</b></p>
+				</div>
+
 				<h2 class = "fr">Web Design</h2>
-				<p class = "fr">Nous conceptualisons votre site web et le prototypons sur FIGMA.</p>
-				<a class = "btn btn_base fr" style = "margin-left: 2rem">Lançons votre projet  &#10140;</a>
+
+				<p class = "fr" style = "margin-bottom: 1rem">Nous conceptualisons votre site web et le prototypons sur FIGMA.</p>
+
+				<div style = "align-items: right">
+					<a class = "btn btn_base center fr" style = "padding: 0.5rem 0.5rem; width: 10rem">Lançons votre projet  &#10140;</a>
+				</div>
 			</div>
+
+			<div class = "margin">
+			</div>
+
+			<div class = "margin">
+			</div>
+
 		</div>
 	</div>
 
 	<div class = "drop_shadow center" style = "margin: 2rem 0; height: 16rem" >
 		<div class = "margin" style = "display: flex; justify-content: space-between">
 			<div class = "margin" style = "display: flex; flex-direction: column">
+				<div style = "display: flex; margin-bottom: 0.5rem">
+					<p class = "violet"><b>Unity</b></p>
+					<p class = "violet" style = "margin: 0 0.5rem"><b>Mobile or PC</b></p>
+				</div>
+
 				<h2 class = "fr">Game Design</h2>
-				<p class = "fr">Nous programmons vos jeux vidéos sur Unity, pour PC ou mobile.</p>
-				<a class = "btn btn_base fr" style = "margin-right: 2rem">Lançons votre projet  &#10140;</a>
+
+				<p class = "fr" style = "margin-bottom: 1rem">Nous programmons vos jeux vidéos sur Unity, pour PC ou mobile.</p>
+
+				<a class = "btn btn_base center fr" style = "padding: 0.5rem 0.5rem; width: 10rem">Lançons votre projet  &#10140;</a>
 			</div>
 
 			<div class = "margin">
