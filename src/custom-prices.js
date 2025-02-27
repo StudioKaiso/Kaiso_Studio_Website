3437a4f4931e396c1618775a1ea4ec0fef5da13a
Stevi60 <tazmo634@gmail.com>
2025-02-27 19:24:24 +0100

    Modified title hierarchy

--- src/custom-prices.js
+++ src/custom-prices.js
@@ -2,8 +2,10 @@ const template = document.createElement("template");
 template.innerHTML = `
 <section style = "width: 100%; background-color: #F2F2F2" id = "price">
 	<div class = "drop_shadow center" style = "background-color: #FFFFFF; margin-bottom: 2rem; height: 16rem" >
-		<h1 class = "w50-60-100 fr" style = "text-align: center">Chaque détail compte.<br>Imaginons ensemble de nouvelles perspectives.</h1>
-		<h1 class = "w50-60-100 eng" style = "text-align: center">Each detail counts.<br>Let's imagine new perspectives together.</h1>
+		<div class = "margin center">
+		<h2 class = "w50-60-100 fr" style = "text-align: center">Chaque détail compte. Imaginons ensemble de nouvelles perspectives.</h2>
+		<h2 class = "w50-60-100 eng" style = "text-align: center">Each detail counts. Let's imagine new perspectives together.</h2>
+		</div>
 	</div>
 
 	<div class = "drop_shadow center" style = "margin: 2rem 0; height: 16rem" >
@@ -14,7 +16,7 @@ template.innerHTML = `
 					<p class = "violet" style = "margin: 0 0.5rem"><b>Charte Graphique</b></p>
 				</div>
 				
-				<h2 class = "fr">Branding</h2>
+				<h3 class = "fr">Branding</h3>
 
 				<p class = "fr" style = "margin-bottom: 1rem">Nous vous aidons à positionner votre marque selon les préférences de vos clients.</p>
 
@@ -37,7 +39,7 @@ template.innerHTML = `
 					<p class = "violet" style = "margin: 0 0.5rem"><b>Prototypage</b></p>
 				</div>
 
-				<h2 class = "fr">Web Design</h2>
+				<h3 class = "fr">Web Design</h3>
 
 				<p class = "fr" style = "margin-bottom: 1rem">Nous conceptualisons votre site web et le prototypons sur FIGMA.</p>
 
@@ -63,7 +65,7 @@ template.innerHTML = `
 					<p class = "violet" style = "margin: 0 0.5rem"><b>Mobile or PC</b></p>
 				</div>
 
-				<h2 class = "fr">Game Design</h2>
+				<h3 class = "fr">Game Design</h3>
 
 				<p class = "fr" style = "margin-bottom: 1rem">Nous programmons vos jeux vidéos sur Unity, pour PC ou mobile.</p>
 
