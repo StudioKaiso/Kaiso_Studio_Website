21fdc2762c272bd50a3c2aa1514228b2c6826ae3
Stevi60 <tazmo634@gmail.com>
2025-02-28 17:53:11 +0100

    Added the english version of the prices section

--- src/custom-prices.js
+++ src/custom-prices.js
@@ -4,7 +4,7 @@ template.innerHTML = `
 	<div class = "drop_shadow center" style = "background-color: #FFFFFF; margin-bottom: 2rem; height: 16rem" >
 		<div class = "margin center">
 		<h2 class = "w50-60-100 fr" style = "text-align: center">Chaque détail <span class = "red" style = "font-size: 1.8rem;">compte</span>. Imaginons ensemble de nouvelles perspectives.</h2>
-		<h2 class = "w50-60-100 eng" style = "text-align: center">Each detail counts. Let's imagine new perspectives together.</h2>
+		<h2 class = "w50-60-100 eng" style = "text-align: center">Each detail <span class = "red" style = "font-size: 1.8rem;">counts</span>. Let's imagine new perspectives together.</h2>
 		</div>
 	</div>
 
@@ -13,53 +13,62 @@ template.innerHTML = `
 			<div class = "margin" style = "width: 35%; display: flex; flex-direction: column">
 				<div style = "display: flex; margin-bottom: 0.5rem">
 					<p class = "violet"><b>Logo</b></p>
-					<p class = "violet" style = "margin: 0 0.5rem"><b>Charte Graphique</b></p>
+					<p class = "violet fr" style = "margin: 0 0.5rem"><b>Charte Graphique</b></p>
+					<p class = "violet eng" style = "margin: 0 0.5rem"><b>Visual Identity</b></p>
 				</div>
 				
-				<h3 class = "fr">Branding</h3>
+				<h3>Branding</h3>
 
 				<p class = "fr" style = "margin-bottom: 1rem">Nous vous aidons à positionner votre marque selon les préférences de vos clients.</p>
+				<p class = "eng" style = "margin-bottom: 1rem">We'll help your position your brand according to the preferences of your clientele.</p>
 
-				<a class = "btn btn_violet center fr" style = "padding: 0.5rem 0.5rem; width: 10rem">Lançons votre projet  &#10140;</a>
+				<a target="_blank" rel="noopener noreferrer" href="https://www.cal.com/kaiso-studio/" class = "btn btn_violet center fr" style = "padding: 0.5rem 0.5rem; width: 10rem">Lançons votre projet &#10140;</a>
+				<a target="_blank" rel="noopener noreferrer" href="https://www.cal.com/kaiso-studio/" class = "btn btn_violet center eng" style = "padding: 0.5rem 0.5rem; width: 12rem">Let's start your project &#10140;</a>
 			</div>
 
 			<div class = "margin" style = "display: flex; justify-content: center; flex-direction: column; width: auto; z-index: 1;">
-				<p style = "color: #F2F2F2; text-align: right">À partir de:</p>
+				<p class = "fr" style = "color: #F2F2F2; text-align: right">À partir de:</p>
+				<p class = "eng" style = "color: #F2F2F2; text-align: right">Starting from:</p>
 
 				<p style = "font-family: 'Righteous', cursive; text-transform: uppercase;font-weight: 500; font-size: 3.5rem;color: #F2F2F2; height: 4rem"><br>2000 €</p>
 			</div>
 		</div>
 
-		<img src = "PriceBackground.2f5eab56.png" style = "height: 16rem; z-index: 1; position: absolute; left: 60%">
-		<div style = "height: 16rem; width: 59%; background-color: #8A94D2; position: absolute; left: 61%"></div>
+		<img src = "PriceBackground.2f5eab56.png" style = "height: 16rem; z-index: 1; position: absolute; left: 55%">
+		<div style = "height: 16rem; width: 54%; background-color: #8A94D2; position: absolute; left: 56%"></div>
 	</div>
 
 	<div class = "drop_shadow center" style = "margin: 2rem 0; height: 16rem" >
 		<div class = "margin" style = "display: flex; flex-direction: row-reverse; justify-content: space-between">
 			<div class = "margin" style = "width: 35%; display: flex; flex-direction: column; text-align: right">
 				<div style = "display: flex; flex-direction: row-reverse; margin-bottom: 0.5rem;">
-					<p class = "violet"><b>Site Web</b></p>
-					<p class = "violet" style = "margin: 0 0.5rem"><b>Prototypage</b></p>
+					<p class = "violet fr"><b>Site Web</b></p>
+					<p class = "violet eng"><b>Website</b></p>
+					<p class = "violet fr" style = "margin: 0 0.5rem"><b>Prototypage</b></p>
+					<p class = "violet eng" style = "margin: 0 0.5rem"><b>Prototyping</b></p>
 				</div>
 
-				<h3 class = "fr">Web Design</h3>
+				<h3>Web Design</h3>
 
 				<p class = "fr" style = "margin-bottom: 1rem">Nous conceptualisons votre site web et le prototypons sur FIGMA.</p>
+				<p class = "eng" style = "margin-bottom: 1rem">We'll conceptualize your website and create prototypes using FIGMA.</p>
 
 				<div style = "align-items: right">
-					<a class = "btn btn_violet center fr" style = "padding: 0.5rem 0.5rem; width: 10rem">Lançons votre projet  &#10140;</a>
+					<a target="_blank" rel="noopener noreferrer" href="https://www.cal.com/kaiso-studio/" class = "btn btn_violet center fr" style = "padding: 0.5rem 0.5rem; width: 10rem">Lançons votre projet &#10140;</a>
+					<a target="_blank" rel="noopener noreferrer" href="https://www.cal.com/kaiso-studio/" class = "btn btn_violet center eng" style = "padding: 0.5rem 0.5rem; width: 12rem">Let's start your project &#10140;</a>
 				</div>
 			</div>
 
 			<div class = "margin" style = "display: flex; justify-content: center; flex-direction: column; width: auto; z-index: 1;">
-				<p style = "color: #F2F2F2">À partir de:</p>
+				<p class = "fr" style = "color: #F2F2F2;">À partir de:</p>
+				<p class = "eng" style = "color: #F2F2F2;">Starting from:</p>
 				
 				<p style = "font-family: 'Righteous', cursive; text-transform: uppercase; font-weight: 500; font-size: 3.5rem;color: #F2F2F2; height: 4rem"><br>1700 €</p>
 			</div>
 		</div>
 
-		<img src = "PriceBackground.2f5eab56.png" style = "transform: scaleX(-1); height: 16rem; z-index: 1; position: absolute; right: 60%">
-		<div style = "height: 16rem; width: 59%; background-color: #8A94D2; position: absolute; right: 61%"></div>
+		<img src = "PriceBackground.2f5eab56.png" style = "transform: scaleX(-1); height: 16rem; z-index: 1; position: absolute; right: 55%">
+		<div style = "height: 16rem; width: 54%; background-color: #8A94D2; position: absolute; right: 56%"></div>
 	</div>
 
 	<div class = "drop_shadow center" style = "margin: 2rem 0; height: 16rem" >
@@ -70,30 +79,33 @@ template.innerHTML = `
 					<p class = "violet" style = "margin: 0 0.5rem"><b>Mobile or PC</b></p>
 				</div>
 
-				<h3 class = "fr">Game Design</h3>
+				<h3>Game Design</h3>
 
 				<p class = "fr" style = "margin-bottom: 1rem">Nous programmons vos jeux vidéos sur Unity, pour PC ou mobile.</p>
+				<p class = "eng" style = "margin-bottom: 1rem">We'll program your video games using Unity, for PC or mobile.</p>
 
-				<a class = "btn btn_violet center fr" style = "padding: 0.5rem 0.5rem; width: 10rem">Lançons votre projet  &#10140;</a>
+				<a target="_blank" rel="noopener noreferrer" href="https://www.cal.com/kaiso-studio/" class = "btn btn_violet center fr" style = "padding: 0.5rem 0.5rem; width: 10rem">Lançons votre projet &#10140;</a>
+				<a target="_blank" rel="noopener noreferrer" href="https://www.cal.com/kaiso-studio/" class = "btn btn_violet center eng" style = "padding: 0.5rem 0.5rem; width: 12rem">Let's start your project &#10140;</a>
 			</div>
 
 			<div class = "margin" style = "display: flex; justify-content: center; flex-direction: column; width: auto; z-index: 1;">
-				<p style = "color: #F2F2F2; text-align: right">À partir de:</p>
+				<p class = "fr" style = "color: #F2F2F2; text-align: right">À partir de:</p>
+				<p class = "eng" style = "color: #F2F2F2; text-align: right">Starting from:</p>
 
 				<p style = "font-family: 'Righteous', cursive; text-transform: uppercase;font-weight: 500; font-size: 3.5rem;color: #F2F2F2; height: 4rem"><br>2000 €</p>
 			</div>
 		</div>
 		
-		<img src = "PriceBackground.2f5eab56.png" style = "height: 16rem; z-index: 1; position: absolute; left: 60%">
-		<div style = "height: 16rem; width: 59%; background-color: #8A94D2; position: absolute; left: 61%"></div>
+		<img src = "PriceBackground.2f5eab56.png" style = "height: 16rem; z-index: 1; position: absolute; left: 55%">
+		<div style = "height: 16rem; width: 54%; background-color: #8A94D2; position: absolute; left: 56%"></div>
 	</div>
 
 	<div class = "drop_shadow center" style = "background-color: #FFFFFF; margin-top: 2rem; height: 16rem" >
 		<div class = "margin center" style = "flex-direction: column;">
 		<h2 class = "w50-60-100 fr" style = "text-align: center">Un devis personnalisé?</h2>
 		<h2 class = "w50-60-100 eng" style = "text-align: center">Need a personalized estimation?</h2>
-		<a class = "btn btn_violet center fr" style = "margin-top: 1.5rem; padding: 0.5rem 0.5rem; width: 10rem">Contactez-nous  &#10140;</a>
-		<a class = "btn btn_violet center eng" style = "margin-top: 1.5rem; padding: 0.5rem 0.5rem; width: 10rem">Contact us  &#10140;</a>
+		<a target="_blank" rel="noopener noreferrer" href="https://www.cal.com/kaiso-studio/" class = "btn btn_violet center fr" style = "margin-top: 1.5rem; padding: 0.5rem 0.5rem; width: 10rem">Contactez-nous  &#10140;</a>
+		<a target="_blank" rel="noopener noreferrer" href="https://www.cal.com/kaiso-studio/" class = "btn btn_violet center eng" style = "margin-top: 1.5rem; padding: 0.5rem 0.5rem; width: 10rem">Contact us  &#10140;</a>
 		</div>
 	</div>
 </section>
